829624ca2fb3409cf958d2d466b00551f6be4aab
Jennifer Fadriquela <jenniferfadriquela@willistowerswatson.com>
2021-10-23 15:17:29 +0800

    add view cert in ui

--- jeng/BCI/bci-web/src/components/CertificateView.js
+++ jeng/BCI/bci-web/src/components/CertificateView.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 
 const CertificateView = () => {
+    const options = {
+        cMapUrl: 'cmaps/',
+        cMapPacked: true,
+    };
+    
     const [details, setDetails] = useState({
         userId: "",
         cid: "",
+        file: null,
+        numPages: 1
     });
 
     const onInputChange = e => {
         setDetails({ ...details, [e.target.name]: e.target.value });
     };
 
-    const { userId, cid } = details;
+    const { userId, cid, file, numPages } = details;
 
     const handleViewCert = async (ev) => {
         ev.preventDefault();
@@ -27,7 +36,30 @@ const CertificateView = () => {
 
         const response = await fetch('api/view-cert', request);
         const responseJson = await response.json();
-        console.log('view cert result', responseJson);
+
+        const base64Param = `data:application/pdf;base64,${responseJson.base64}`;
+        const file = dataURLtoFile(base64Param,'cert.pdf');
+
+        setDetails({ ...details, file });
+    }
+
+    const dataURLtoFile = (dataurl, filename) => {
+ 
+        var arr = dataurl.split(','),
+            mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), 
+            n = bstr.length, 
+            u8arr = new Uint8Array(n);
+            
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        
+        return new File([u8arr], filename, {type:mime});
+    }
+
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setDetails({ ...details, numPages });
     }
     
     return (
@@ -48,6 +80,23 @@ const CertificateView = () => {
                 </div>
                 <button type="submit" class="btn btn-primary mt-4">View</button>
             </form>
+
+            <Document
+                file={file}
+                onLoadSuccess={onDocumentLoadSuccess}
+                options={options}>
+                {
+                    Array.from(
+                        new Array(numPages),
+                        (el, index) => (
+                        <Page
+                            key={`page_${index + 1}`}
+                            pageNumber={index + 1}
+                        />
+                        ),
+                    )
+                }
+            </Document>
         </div>
     );
 }
